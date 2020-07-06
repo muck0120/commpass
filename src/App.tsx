d89d42da4c87d15de4bcef8c9fa0564992f80020
sawadamutsuki <mutsuki.s.0120@gmail.com>
2020-07-07 08:36:27 +0900

    [add]test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import { Redirect, Route, Switch } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './Home';
+import Event from './Event';
+
+const App: FC<{}> = () => (
+  <div className="container">
+    <Switch>
+      <Route path="/event" component={Event} />
+      <Route path="/" component={Home} />
+      <Redirect to="/" />;
+    </Switch>
+  </div>
+);
 
 export default App;
---
+++ src/Event.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const Event: FC<{}> = () => (
+    <>
+      <h1>イベント詳細</h1>
+    </>
+);
+
+export default Event;
