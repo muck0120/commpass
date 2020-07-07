01ae420ee221f1ef9a6797a23673368cee278727
Mutsuki Sawada <mutsuki.s.0120@gmail.com>
2020-07-07 22:14:02 +0900

    [add]不要ファイル削除

--- src/Event.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-const Event: FC<{}> = () => (
-    <>
-      <h1>イベント詳細</h1>
-    </>
-);
-
-export default Event;
--- src/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-const Home: FC<{}> = () => (
-    <>
-      <h1>ホーム</h1>
-    </>
-);
-
-export default Home;
