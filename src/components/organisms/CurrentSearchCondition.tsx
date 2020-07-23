92fc68fd9a4638c94e9616086efe682003580deb
Mutsuki Sawada <mutsuki.s.0120@gmail.com>
2020-07-23 18:34:31 +0900

    [add]「現在の検索条件」のコンポーネントのロジック実装

--- src/components/organisms/CurrentSearchCondition.tsx
+++ src/components/organisms/CurrentSearchCondition.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
+import { useLocation } from 'react-router-dom';
+import { animateScroll as scroll } from 'react-scroll';
 
 import styles from 'styles/components/organisms/CurrentSearchCondition.module.scss';
 
 const CurrentSearchCondition: FC = () => {
+  const params = new URLSearchParams(useLocation().search);
   return (
     <div className={styles.container}>
       <div className={styles.body}>
@@ -10,22 +13,34 @@ const CurrentSearchCondition: FC = () => {
         <ul className={styles.content}>
           <li className={styles.list}>
             <span className={styles.title}>都道府県：</span>
-            <span className={styles.data}>東京</span>
+            <span className={styles.data}>
+              {params.get('prefecture') || 'すべて'}
+            </span>
           </li>
           <li className={styles.list}>
             <span className={styles.title}>並び順：</span>
-            <span className={styles.data}>新着順</span>
+            <span className={styles.data}>
+              {params.get('orderBy') === '2'
+                ? '開催日時順'
+                : params.get('orderBy') === '3'
+                ? '新着順'
+                : '更新日時順'}
+            </span>
           </li>
           <li className={styles.list}>
             <span className={styles.title}>言語・技術：</span>
             <span className={styles.data}>
-              HTML, CSS, JavaScript, Java, PHP, Ruby, Python
+              {params.get('languages')
+                ? params.get('languages')?.split(',').join(', ')
+                : 'すべて'}
             </span>
           </li>
           <li className={styles.list}>
             <span className={styles.title}>フレームワーク：</span>
             <span className={styles.data}>
-              Ruby on Rails, Laravel, Express, CakePHP, Vue, React
+              {params.get('frameworks')
+                ? params.get('frameworks')?.split(',').join(', ')
+                : 'すべて'}
             </span>
           </li>
           <li className={styles.list}>
@@ -35,12 +50,20 @@ const CurrentSearchCondition: FC = () => {
           <li className={styles.list}>
             <span className={styles.title}>キーワード：</span>
             <span className={styles.data}>
-              あいうえお, かきくけこ, テストテスト, ダミーテキスト
+              {params.get('keywords')
+                ? params.get('keywords')?.split(',').join(', ')
+                : '-'}
             </span>
           </li>
         </ul>
       </div>
-      <div className={styles.button}>条件を変更する</div>
+      <button
+        onClick={() => scroll.scrollToTop({ duration: 500 })}
+        type="button"
+        className={styles.button}
+      >
+        条件を変更する
+      </button>
     </div>
   );
 };
