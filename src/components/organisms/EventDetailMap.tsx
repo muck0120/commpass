c3184f5290cdb7c3c41787e934c85ed7b1ee75fd
Mutsuki Sawada <mutsuki.s.0120@gmail.com>
2020-07-29 14:30:21 +0900

    [add]イベント詳細ページのマップコンポーネントへロジック実装

--- src/components/organisms/EventDetailMap.tsx
+++ src/components/organisms/EventDetailMap.tsx
@@ -1,12 +1,40 @@
 import React, { FC } from 'react';
+import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api';
 
+import { Event } from 'apis/connpassAPI';
 import styles from 'styles/components/organisms/EventDetailMap.module.scss';
 
-const EventDetailContent: FC = () => {
+interface EventDetailMapProps {
+  event: Event;
+}
+
+const EventDetailContent: FC<EventDetailMapProps> = ({
+  event: { lat, lon: lng },
+}) => {
+  const position = { lat: Number(lat), lng: Number(lng) };
+  const mapStyle = { width: '100%', height: '100%' };
+
   return (
     <div className={styles.container}>
       <h3 className={styles.heading}>開催場所</h3>
-      <div className={styles.content}>{/*  */}</div>
+      <div className={styles.content}>
+        <LoadScript
+          googleMapsApiKey={process.env.REACT_APP_APP_GOOGLE_MAPS_API_KEY}
+        >
+          <GoogleMap
+            mapContainerStyle={mapStyle}
+            zoom={15}
+            center={position}
+            options={{
+              mapTypeControl: false,
+              fullscreenControl: false,
+              streetViewControl: false,
+            }}
+          >
+            <Marker position={position} />
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 };
